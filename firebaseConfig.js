13aa3bcaa2f8c979c56bd6fba31f838424d8b423
Dancing bull <72798082+upscaspirantmzr@users.noreply.github.com>
2025-09-25 20:40:42 +0530

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+     apiKey: "AIzaSyCzlbVpsLSQECmVUs5wyaXKy-fz8nfhjkM", // Replace with your actual API Key
+            authDomain: "schooladmsn.firebaseapp.com",
+            projectId: "schooladmsn",
+            storageBucket: "schooladmsn.firebasestorage.app",
+            messagingSenderId: "355479237462",
+            appId: "1:355479237462:web:8eaba019c00ad8684d5cf7",
+            measurementId: "G-LJW1ZQTGSV"
+};
